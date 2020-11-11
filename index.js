7ee12d6237b2c3ace53146929a7b56ca13222739
kennedy.ikeka <kennedy.ikeka@ipigroupng.com>
2020-11-11 13:20:57 +0100

    Initialization Complete

--- index.js
+++ index.js
@@ -20,20 +20,37 @@ let { port, protocol } = bankServer.getCommands('-');
 if (!base.isset(port)) port = 8082;
 if (!base.isset(protocol)) protocol = 'https';
 
-bankServer.createServer({
-    port,
-    protocol,
-    allow: { origins: ['*'] },
-    httpsOptions: {
-        key: fs.readFileSync('./permissions/server.key'),
-        cert: fs.readFileSync('./permissions/server.crt')
-    },
-    response: params => {
-        params.response.end('View');
-    }
-});
+function main() {
 
-bankServer.recordSession({ period: 24 * 60 * 60 * 1000, remember: ['user'], server: serverDetails });
+    db.find({ collection: 'staff', query: { type: 'admin' } }).then(staff => {
+        if (!staff) {
+            let data = { currentPassword: 'admin@1234', type: 'admin', userName: '@admin' };
+            bcrypt.hash(data.currentPassword, 10).then(hash => {
+                data.currentPassword = hash;
+                db.insert({ collection: 'staff', query: data }).then(done => {
+                    console.log("Initialization Complete");
+                })
+            });
+        }
+    });
 
-bankServer.methods.post = (req, res, form) => postManager.act(req, res, form);
-bankServer.makeStatic('app/public');
+    bankServer.createServer({
+        port,
+        protocol,
+        allow: { origins: ['*'] },
+        httpsOptions: {
+            key: fs.readFileSync('./permissions/server.key'),
+            cert: fs.readFileSync('./permissions/server.crt')
+        },
+        response: params => {
+            params.response.end('View');
+        }
+    });
+
+    bankServer.recordSession({ period: 24 * 60 * 60 * 1000, remember: ['user'], server: serverDetails });
+
+    bankServer.methods.post = (req, res, form) => postManager.act(req, res, form);
+    bankServer.makeStatic('app/public');
+}
+
+main();
